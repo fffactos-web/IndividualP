4384238379ef072a81c9892e332e7d13d9b9439f
fffactos-web <fffactos@gmail.com>
2026-03-17 16:48:37 +0100

    Fix camera toggle animation and weapon visibility

--- Assets/Scripts/CameraBrains.cs
+++ Assets/Scripts/CameraBrains.cs
@@ -3,22 +3,18 @@ using UnityEngine;
 
 public class CameraBrains : MonoBehaviour
 {
-
     bool isOn = true;
     public float crosshairY = 40;
 
-    [SerializeField]
-    CinemachineVirtualCamera firstPersonCam;
+    [SerializeField] CinemachineVirtualCamera firstPersonCam;
+    [SerializeField] GameObject[] healthBars;
+    [SerializeField] Animator animator;
 
-    [SerializeField]
-    GameObject[] healthBars;
-
-    [SerializeField]
-    Animator animator;
-
-    GameObject gunHolder;
+    GameObject firstPersonGunHolder;
+    GameObject thirdPersonGunHolder;
     CinemachineVirtualCamera cam;
     GameObject character;
+    Renderer[] characterRenderers;
     RectTransform rect;
     Movement movement;
 
@@ -26,60 +22,110 @@ public class CameraBrains : MonoBehaviour
     {
         cam = GetComponent<CinemachineVirtualCamera>();
         character = GameObject.FindGameObjectWithTag("Character");
-        gunHolder = GameObject.FindGameObjectWithTag("Camera Gun");
-        movement = GameObject.FindGameObjectWithTag("Player").GetComponent<Movement>();
-        rect = GameObject.FindGameObjectWithTag("Crosshair").GetComponent<RectTransform>();
-        animator = character.GetComponent<Animator>();
+        if (character != null)
+        {
+            characterRenderers = character.GetComponentsInChildren<Renderer>(true);
+            animator = character.GetComponent<Animator>();
+            if (animator != null)
+                animator.cullingMode = AnimatorCullingMode.AlwaysAnimate;
+        }
+
+        movement = GameObject.FindGameObjectWithTag("Player")?.GetComponent<Movement>();
+        rect = GameObject.FindGameObjectWithTag("Crosshair")?.GetComponent<RectTransform>();
+
+        TryResolveGunHolders();
     }
 
     void Update()
     {
+        TryResolveGunHolders();
+
         if (Input.GetAxis("Mouse ScrollWheel") < 0 && cam.GetCinemachineComponent<CinemachineFramingTransposer>().m_CameraDistance < 10 && isOn)
         {
             cam.GetCinemachineComponent<CinemachineFramingTransposer>().m_CameraDistance += 1f;
-            rect.localPosition += new Vector3(0, -20, 0);
+            if (rect != null)
+                rect.localPosition += new Vector3(0, -20, 0);
             crosshairY += -20f;
         }
         else if (Input.GetAxis("Mouse ScrollWheel") > 0 && cam.GetCinemachineComponent<CinemachineFramingTransposer>().m_CameraDistance > 2 && isOn)
         {
             cam.GetCinemachineComponent<CinemachineFramingTransposer>().m_CameraDistance += -1f;
-            rect.localPosition += new Vector3(0, 20, 0);
+            if (rect != null)
+                rect.localPosition += new Vector3(0, 20, 0);
             crosshairY += 20f;
         }
 
         if (Input.GetKeyDown(KeyCode.V))
         {
-            if(isOn)
+            if (isOn)
             {
-                gunHolder.SetActive(true);
-                rect.localPosition = new Vector3(0, 0, 0);
+                if (firstPersonGunHolder != null)
+                    firstPersonGunHolder.SetActive(true);
+                if (thirdPersonGunHolder != null)
+                    thirdPersonGunHolder.SetActive(false);
+
+                if (rect != null)
+                    rect.localPosition = new Vector3(0, 0, 0);
+
                 firstPersonCam.GetCinemachineComponent<CinemachinePOV>().m_VerticalAxis = cam.GetCinemachineComponent<CinemachinePOV>().m_VerticalAxis;
                 firstPersonCam.GetCinemachineComponent<CinemachinePOV>().m_HorizontalAxis = cam.GetCinemachineComponent<CinemachinePOV>().m_HorizontalAxis;
                 cam.m_Priority += -2;
-                character.SetActive(false);
+                SetCharacterVisible(false);
                 isOn = false;
+
                 foreach (var bar in healthBars)
                     if (bar != null)
                         bar.SetActive(false);
-                
             }
             else
             {
-                gunHolder.SetActive(false);
-                rect.localPosition = new Vector3(0, crosshairY, 0);
+                if (firstPersonGunHolder != null)
+                    firstPersonGunHolder.SetActive(false);
+                if (thirdPersonGunHolder != null)
+                    thirdPersonGunHolder.SetActive(true);
+
+                if (rect != null)
+                    rect.localPosition = new Vector3(0, crosshairY, 0);
+
                 cam.GetCinemachineComponent<CinemachinePOV>().m_VerticalAxis = firstPersonCam.GetCinemachineComponent<CinemachinePOV>().m_VerticalAxis;
                 cam.GetCinemachineComponent<CinemachinePOV>().m_HorizontalAxis = firstPersonCam.GetCinemachineComponent<CinemachinePOV>().m_HorizontalAxis;
                 cam.m_Priority += 2;
-                character.SetActive(true);
+                SetCharacterVisible(true);
                 isOn = true;
+
                 foreach (var bar in healthBars)
-                    if(bar != null)
+                    if (bar != null)
                         bar.SetActive(true);
-                animator.SetBool("Run", movement.isRunning);
-                animator.SetBool("Jump", movement.inAir);
-                animator.SetBool("Fire", movement.isFiring); 
-                animator.SetFloat("Speed", movement.animatorSpeed);
+
+                if (animator != null && movement != null)
+                {
+                    animator.SetBool("Run", movement.isRunning);
+                    animator.SetBool("Jump", movement.inAir);
+                    animator.SetBool("Fire", movement.isFiring);
+                    animator.SetFloat("Speed", movement.animatorSpeed);
+                }
             }
+        }
+    }
+
+    void TryResolveGunHolders()
+    {
+        if (firstPersonGunHolder == null)
+            firstPersonGunHolder = GameObject.FindGameObjectWithTag("Camera Gun");
+
+        if (thirdPersonGunHolder == null)
+            thirdPersonGunHolder = GameObject.FindGameObjectWithTag("Gunn");
+    }
+
+    void SetCharacterVisible(bool isVisible)
+    {
+        if (characterRenderers == null)
+            return;
+
+        foreach (var item in characterRenderers)
+        {
+            if (item != null)
+                item.enabled = isVisible;
         }
     }
 }
